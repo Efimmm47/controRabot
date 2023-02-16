a575f3a0b91cfdc3b41e4ed72949101c5326e9f4
Efim Malyy <she3442bdu@gmail.com>
2023-02-16 22:31:47 +0800

    progamm

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите массив строк через пробел");
+string inPutString = Console.ReadLine();
+string[] userArray = inPutString.Split(' ');
+
+string[] resultArray = new string[userArray.Length];
+
+SearchElemnentsBigerThenFour(userArray, resultArray);
+Console.Write($"Элементы со значением меньше либо равно 3 символам: ");
+PrintArray(resultArray);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SearchElemnentsBigerThenFour(string[] inputArray, string[] resultArray)
+{
+    int count = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length < 4)
+        {
+            resultArray[count] = inputArray[i];
+            count++;
+        }
+    }
+}
